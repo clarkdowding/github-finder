7384c0bd80a071d574a4a4679b6ae38aa4dbe661
ClarkD <clarkdowding@mac.com>
2020-09-22 18:23:46 -0600

    Add user components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Sample from './Sample';
+// import Sample from './Sample';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Sample />
   </React.StrictMode>,
   document.getElementById('root')
 );
