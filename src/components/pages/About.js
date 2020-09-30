b637a7f1fc84d7158f84a08a13171479622e44b7
ClarkD <clarkdowding@mac.com>
2020-09-30 11:54:01 -0600

    Change to v2.0 for contextual state version

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <Fragment>
             <h1>About this App</h1>
             <p>App to search Github users</p>
-            <p>Vesion: 1.0.0</p>
+            <p>Vesion: 2.0.0</p>
         </Fragment>
     )
 }
